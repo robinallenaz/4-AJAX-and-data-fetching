8aadf80c06aaecda444e1577e6429bf211ef00b2
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2025-02-24 22:27:15 -0600

    Improve code comments for clarity

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
+// This script demonstrates the use of AJAX with the XMLHttpRequest object to fetch data from a server.
+// It includes examples of GET requests and handling server responses.
+
 console.log('AJAX!');
 
 ///////////////////////////////
 // The XMLHttpRequest Object
 ///////////////////////////////
 
+// Create a new XMLHttpRequest object
 // const request = new XMLHttpRequest();
 
+// Log the request object to the console
 // console.log(request);
 
-// Example
+// Example of opening a request
 // request.open('GET', 'http://www.example.com/myFile.json', true);
 
-//if this were a POST request, and there is data you need to send to the server you need to set the request header to specify the type of data you're sending
-// only for when you send data with a request
+// If this were a POST request, and there is data you need to send to the server,
+// you need to set the request header to specify the type of data you're sending.
+// This is only necessary when sending data with a request.
 // request.setRequestHeader(
 //   "Content-Type",
 //   "application/json"
 // );
 
+// Send the request
 // request.send();
 
-/// Simple XHR Requst
+/// Simple XHR Request
 let request;
 
+// Add an event listener to a button with id 'myBtn'
 document.getElementById('myBtn').addEventListener('click', testRequest);
 
+// Function to test the XMLHttpRequest
 function testRequest() {
+  // Create a new XMLHttpRequest object
   request = new XMLHttpRequest();
 
+  // Check if the request object was created successfully
   if (!request) {
     alert('Failed to create an XMLHttpRequest Object.');
     return false;
   }
 
+  // Set the function to be called when the state of the request changes
   request.onreadystatechange = alertResponse;
+  // Open a GET request to 'test.html'
   request.open('GET', 'test.html');
+  // Send the request
   request.send();
 }
 
+// Function to handle the response from the server
 function alertResponse() {
+  // Check if the request is complete
   if (request.readyState === XMLHttpRequest.DONE) {
+    // Check if the request was successful
     if (request.status === 200) {
+      // Alert the response text
       alert(request.responseText);
     } else {
+      // Handle errors
       handleError(request.status);
     }
   }
 }
 
+// Function to handle errors
 function handleError(status) {
   let errorMessage = 'An error occurred: ';
   switch (status) {
