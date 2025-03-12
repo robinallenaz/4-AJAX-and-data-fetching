3b45047d9446c75472292b7e112c396b6918b070
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2025-03-11 20:57:29 -0500

    Refactor script.js

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-// This script demonstrates the use of AJAX with the XMLHttpRequest object to fetch data from a server.
-// It includes examples of GET requests and handling server responses.
-
+// Improved script demonstrating AJAX with Fetch API
 console.log('AJAX!');
 
 ///////////////////////////////
-// The XMLHttpRequest Object
+// Fetch API Example
 ///////////////////////////////
 
-// Create a new XMLHttpRequest object
-// const request = new XMLHttpRequest();
-
-// Log the request object to the console
-// console.log(request);
-
-// Example of opening a request
-// request.open('GET', 'http://www.example.com/myFile.json', true);
-
-// If this were a POST request, and there is data you need to send to the server,
-// you need to set the request header to specify the type of data you're sending.
-// This is only necessary when sending data with a request.
-// request.setRequestHeader(
-//   "Content-Type",
-//   "application/json"
-// );
-
-// Send the request
-// request.send();
-
-/// Simple XHR Request
-let request;
-
 // Add an event listener to a button with id 'myBtn'
-document.getElementById('myBtn').addEventListener('click', testRequest);
+document.getElementById('myBtn').addEventListener('click', () => {
+  fetchData('test.html');
+});
 
-// Refactored testRequest function using Fetch API
-function testRequest() {
-  fetch('test.html')
+// Function to fetch data
+function fetchData(url) {
+  fetch(url)
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -48,13 +25,19 @@ function testRequest() {
 
 // Function to handle errors
 function handleError(error) {
+  console.error(`An error occurred: ${error}`);
   alert(`An error occurred: ${error}`);
 }
 
+// Function to log JSON data
 async function logJSONData() {
-    const response = await fetch('HTTPS://jsonplaceholder.typicode.com/todos/2');
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/2');
     const jsonData = await response.json();
     console.log(jsonData);
+  } catch (error) {
+    handleError(error.message);
+  }
 }
 
 logJSONData();
